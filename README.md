38aa5eadcee3db915437fe6bf10c84db554d6b62
Preston Ong <preston4896@me.com>
2020-10-13 13:05:22 -0700

    Added quicksort

--- README.md
+++
@@ -1,5 +0,0 @@
-# C++ Data Structure and Algorithm by Preston Ong
-
-This project contains data structures and algorithms inplemented using C++.
-
-Last Updated: July 25th, 2020
---
+++ algo/quick_sort.py
@@ -0,0 +1,27 @@
+import math
+
+A = [10, 16, 8, 12, 15, 6, 3, 9, 5]
+
+def partition(l,h):
+    pivot = A[l]
+    i = l
+    j = h
+    while (i < j):
+        while (A[i] <= pivot):
+            i += 1
+        while (A[j] > pivot):
+            j -= 1
+        if (i < j):
+            A[i], A[j] = A[j], A[i]
+    A[l], A[j] = A[j], A[l]
+    return j
+
+def quicksort(l,h):
+    if (l < h):
+        j = partition(l, h)
+        quicksort(l, j)
+        quicksort(j+1, h)
+
+print("Input list: ", A)
+quicksort(0, len(A) - 1)
+print("Sorted list: ", A)
